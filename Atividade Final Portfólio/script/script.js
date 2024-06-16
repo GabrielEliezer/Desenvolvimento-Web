daa548a1478491726c046c1dde882195cbeade74
GabrielEliezer <g.eliezer.rodrigues@gmail.com>
2024-06-16 17:05:31 -0300

    Ultima atualização da atividade do portfólio

--- Atividade Final Portfólio/script/script.js
+++ Atividade Final Portfólio/script/script.js
@@ -1,84 +1,77 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let item = 0; // Variável para controlar a ação conforme o botão clicado
+    var item = 0; // Variavel de controle para o if 
 
-    let buttons = document.getElementsByClassName('button');
+    var buttons = document.getElementsByClassName('button');
 
-    // Adicionando evento de clique para cada botão
-    for (let i = 0; i < buttons.length; i++) {
+    // for para entender qual botao foi clicado
+    for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function() {
-            // Remover a classe 'teste' de todos os botões
-            for (let j = 0; j < buttons.length; j++) {
+            // Tira a cor colorida do botão quando mudar clicar em outro
+            for (var j = 0; j < buttons.length; j++) {
                 buttons[j].classList.remove('teste');
             }
-            // Adicionar a classe 'teste' apenas ao botão clicado
-            buttons[i].classList.add('teste');
+            // Mantem o botao colorido quando for clicado
+            this.classList.add('teste');
 
-            // Remover iframe existente
-            let existingIframe = document.querySelector('.iframe');
+            // Remove o Iframe Anterior
+            var existingIframe = document.querySelector('.iframe');
             if (existingIframe) {
                 existingIframe.remove();
             }
 
-            // Determinar a ação com base no botão clicado
-            switch(buttons[i].id) {
-                case 'automacao':
-                    if (item !== 1) {
-                        console.log('Automação');
-                        item = 1;
+            // Com base no botão clicado ele escolhe qual if vai ser verdadeiro
+            if (this.id === 'automacao') { //verifica se o elemento que acionou o evento é automação
+                if (item !== 1) {
+                    console.log('Automação');
+                    item = 1;
 
-                        var ifrm = document.createElement("iframe");
-                        ifrm.setAttribute("src", "At.html");
-                        document.getElementsByClassName('body')[0].appendChild(ifrm);
-                        ifrm.classList.add('iframe');
+                    var ifrm = document.createElement("iframe");
+                    ifrm.setAttribute("src", "At.html");
+                    document.getElementsByClassName('body')[0].appendChild(ifrm);
+                    ifrm.classList.add('iframe');
+                }
+            } else if (this.id === 'banco_de_dados') {//verifica se o elemento que acionou o evento é Banco de Dados
+                if (item !== 2) {
+                    console.log('Banco de Dados'); // Coloquei para imprimir no console para conferir se estava caindo no If
+                    item = 2;
 
-                    }
-                    break;
-                case 'banco_de_dados':
-                    if (item !== 2) {
-                        console.log('Banco de Dados');
-                        item = 2;
+                    var ifrm = document.createElement("iframe");
+                    ifrm.setAttribute("src", "Bd.html");
+                    document.getElementsByClassName('body')[0].appendChild(ifrm);
+                    ifrm.classList.add('iframe');
+                }
+            } else if (this.id === 'ciencia_de_dados') {//verifica se o elemento que acionou o evento é Ciencia de Dados
+                if (item !== 3) {
+                    console.log('Ciência de Dados');
+                    item = 3;
 
-                        var ifrm = document.createElement("iframe");
-                        ifrm.setAttribute("src", "Bd.html");
-                        document.getElementsByClassName('body')[0].appendChild(ifrm);
-                        ifrm.classList.add('iframe');
-                    }
-                    break;
-                case 'ciencia_de_dados':
-                    if (item !== 3) {
-                        console.log('Ciência de Dados');
-                        item = 3;
+                    var ifrm = document.createElement("iframe");
+                    ifrm.setAttribute("src", "Cd.html");
+                    document.getElementsByClassName('body')[0].appendChild(ifrm);
+                    ifrm.classList.add('iframe');
+                }
+            } else if (this.id === 'desenvolvimento_web') {//verifica se o elemento que acionou o evento é Desenvolvimento Web
+                if (item !== 4) {
+                    console.log('Desenvolvimento Web');
+                    item = 4;
 
-                        var ifrm = document.createElement("iframe");
-                        ifrm.setAttribute("src", "Cd.html");
-                        document.getElementsByClassName('body')[0].appendChild(ifrm);
-                        ifrm.classList.add('iframe');
-                    }
-                    break;
-                case 'desenvolvimento_web':
-                    if (item !== 4) {
-                        console.log('Desenvolvimento Web');
-                        item = 4;
+                    var ifrm = document.createElement("iframe");
+                    ifrm.setAttribute("src", "Dw.html");
+                    document.getElementsByClassName('body')[0].appendChild(ifrm);
+                    ifrm.classList.add('iframe');
+                }
+            } else if (this.id === 'linguagem_de_programacao') {//verifica se o elemento que acionou o evento é Linguagem de Programação
+                if (item !== 5) {
+                    console.log('Linguagem de Programação');
+                    item = 5;
 
-                        var ifrm = document.createElement("iframe");
-                        ifrm.setAttribute("src", "Dw.html");
-                        document.getElementsByClassName('body')[0].appendChild(ifrm);
-                        ifrm.classList.add('iframe');
-                    }
-                    break;
-                case 'linguagem_de_programacao':
-                    if (item !== 5) {
-                        console.log('Linguagem de Programação');
-                        item = 5;
-
-                        var ifrm = document.createElement("iframe");
-                        ifrm.setAttribute("src", "Lp.html");
-                        document.getElementsByClassName('body')[0].appendChild(ifrm);
-                        ifrm.classList.add('iframe');
-                    }
-                    break;
-                default:
-                    console.log('Botão não reconhecido');
+                    var ifrm = document.createElement("iframe");
+                    ifrm.setAttribute("src", "Lp.html");
+                    document.getElementsByClassName('body')[0].appendChild(ifrm);
+                    ifrm.classList.add('iframe');
+                }
+            } else {
+                console.log('Botão não reconhecido');
             }
         });
     }
